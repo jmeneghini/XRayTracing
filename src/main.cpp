a672b4fc21ec4d81ffbc0eb009f4d5bfdce31c4b
jmeneghini <meneghinijohn@gmail.com>
2023-01-31 00:07:54 -0500

    added more renders

--- src/main.cpp
+++ src/main.cpp
@@ -21,18 +21,27 @@ float ray_intensity(const ray& r, const hittable& world) {
 int main() {
 
     // Image
-    const float aspect_ratio = 16.0 / 9.0;
+    const float aspect_ratio = 1.0/1.0;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // Camera
+    const float viewport_height = 18.0; // in cm
+    const float viewport_width = aspect_ratio * viewport_height;
+    const float focal_length = 100; // in cm
+
     // World
     hittable_list world; // list of objects in the world;
-    world.add(make_shared<mesh>("stl/ancient_chinese_coin.stl", vec3(0, 0, 0),  make_shared<material>(3.148E-01, 1.8))); // bone
-
-    // Camera
-    const float viewport_height = 12.0;
-    const float viewport_width = aspect_ratio * viewport_height;
-    const float focal_length = 6.0;
+//    world.add(make_shared<mesh>("stl/glass_slab.stl", vec3(0, 0, -focal_length),
+//                                make_shared<material>(1.890E-01, 2.23))); // pyrex glass at 80 keV
+//    world.add(make_shared<mesh>("stl/plastic_slab.stl", vec3(0, 0, -focal_length),
+//                                make_shared<material>(1.751E-01, 1.19000E+00))); // acrylic at 80 keV
+//    world.add(make_shared<mesh>("stl/plastic_container.stl", vec3(0, 0, -focal_length),
+//                                make_shared<material>(1.751E-01, 1.19000E+00))); //  acrylic container at 80 keV
+//    world.add(make_shared<mesh>("stl/plastic_container_liquid.stl", vec3(0, 0, -focal_length),
+//                                make_shared<material>(1.837E-01, 1.0))); //  water at 80 keV
+    world.add(make_shared<mesh>("stl/aluminum_G.stl", vec3(0, 0, -focal_length),
+                                make_shared<material>(2.018E-01, 2.699E+00))); //  aluminum at 80 keV
 
     vec3 origin = vec3(0, 0, 0);
     vec3 horizontal = vec3(viewport_width, 0, 0);
@@ -41,7 +50,7 @@ int main() {
 
     // Render
     std::ofstream render;
-    render.open("examples/ancient_chinese_coin.pgm"); // open pgm file for writing greyscale image
+    render.open("examples/test.pgm"); // open pgm file for writing greyscale image
     render << "P2\n" << image_width << ' ' << image_height << "\n255\n";
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
