a6de74ee8a52feace7643ffef998771ce821e602
jmeneghini <meneghinijohn@gmail.com>
2023-03-01 23:42:35 -0500

    fixed config support

--- include/camera.h
+++ include/camera.h
@@ -6,23 +6,22 @@
 class camera {
 public:
     camera() {}
-    camera(int image_width, float viewport_width, float aspect_ratio, float focal_length) {
-        float viewport_height = viewport_width * aspect_ratio;
+    camera(float viewport_width, float aspect_ratio, float focal_length) {
+        float viewport_height = viewport_width / aspect_ratio;
 
 
-        vec3 origin = vec3(0, 0, 0);
-        vec3 horizontal = vec3(viewport_width, 0, 0);
-        vec3 vertical = vec3(0, viewport_height, 0);
-        vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+        origin = vec3(0, 0, 0);
+        horizontal = vec3(viewport_width, 0, 0);
+        vertical = vec3(0, viewport_height, 0);
+        lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
 
 
 
     }
     ray get_ray(float u, float v) const {
-        return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+        return ray(origin, lower_left_corner + u*horizontal + v*vertical);
     }
 private:
-
     vec3 origin;
     vec3 lower_left_corner;
     vec3 horizontal;
